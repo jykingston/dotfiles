ccb5f06de2f0ef70b6064defbe582b9b8b39d03d
jy <Jy Kingston>
2018-11-22 22:38:28 +1100

    many tricks added

--- scripts/copy-dotfiles.sh
+++ scripts/copy-dotfiles.sh
@@ -12,7 +12,7 @@ i3config="$configDir/i3/config"
 i3blocksConfig="$configDir/i3/i3blocks.conf"
 zshConfig="$homeDir/.zshrc"
 tmuxConfig="$homeDir/.tmux/.tmux.conf"
-
+$i3lock="$homeDir/.local/bin/i3exit"
 
 echo "Copying i3config...";
 cat $i3config > $githubDir/i3/i3/config
@@ -22,6 +22,9 @@ echo "Copying zshrc...";
 cat $zshConfig > $githubDir/zsh/.zshrc
 echo "Copying tmux config...";
 cat $tmuxConfig > $githubDir/tmux/.tmux.conf
+#echo "Copying i3lock config...";
+#cat $i3lock > $githubDir/i3/i3/i3exit
+#cat /home/meitis/.local/bin/i3exit > github/dotfiles/i3/i3exit 
 echo "done!";
 echo "Replicating myself into github for future replicating...";
 cat $homeDir/scripts/copy-dotfiles.sh > $githubDir/scripts/copy-dotfiles.sh
