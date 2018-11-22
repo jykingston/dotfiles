e07d38ea0af722d3f3ec9c86ee1c2be16347812f
jy <Jy Kingston>
2018-11-22 21:49:06 +1100

    add dotfile copy tool

---
+++ scripts/copy-dotfiles.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#this tool copys my dotfiles to github
+
+configDir='/home/meitis/.config'
+binDir='/usr/local/bin/'
+githubDir='/home/meitis/github/dotfiles'
+homeDir='/home/meitis'
+
+
+i3config="$configDir/i3/config"
+i3blocksConfig="$configDir/i3/i3blocks.conf"
+zshConfig="$homeDir/.zshrc"
+tmuxConfig="$homeDir/.tmux/.tmux.conf"
+
+
+echo "Copying i3config...";
+cat $i3config > $githubDir/i3/i3/config
+echo "Copying i3blocks config...";
+cat $i3blocksConfig > $githubDir/i3/i3/i3blocks.conf
+echo "Copying zshrc...";
+cat $zshConfig > $githubDir/zsh/.zshrc
+echo "Copying tmux config...";
+cat $tmuxConfig > $githubDir/tmux/.tmux.conf
+echo "done!";
+echo "Replicating myself into github for future replicating...";
+cat $homeDir/scripts/copy-dotfiles.sh > $githubDir/scripts/copy-dotfiles.sh
+echo "done!";
+cd $githubDir
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -3,7 +3,7 @@
 
 # Path to your oh-my-zsh installation.
   export ZSH=/home/meitis/.oh-my-zsh
-
+  export DISABLE_AUTO_TITLE='true'
   # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
